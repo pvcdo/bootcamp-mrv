99a1687cb831606fd674e722c20502db4ef72f9e
PAULO VICTOR CAETANO DE OLIVEIRA PR114067 <paulocivictor@gmail.com>
2021-12-14 16:38:38 -0300

    falta fazer a atividade de 3_INTRO_JAVASCRIPT

--- 1_SINTAXE_OPERADORES/atividade/index.js
+++ 1_SINTAXE_OPERADORES/atividade/index.js
@@ -1,24 +1,28 @@
 function compararSomarComparar(a,b){
-    const comparacao_inputs = a === b ? '' : 'não '
+    if(a && b){
+        const comparacao_inputs = a === b ? '' : 'não '
     
-    const soma = a+b
-    
-    let comparacao_soma = ''
-    
-    if(soma < 10){
-        comparacao_soma = 'menor que 10'
-    }else if(soma > 20){
-        comparacao_soma = 'maior que 20'
-    }else if(soma === 10){
-        comparacao_soma = 'igual a 10'
-    }else if(soma === 20){
-        comparacao_soma = 'igual a 20'
-    }else{
-        comparacao_soma = 'maior que 10 e menor que 20'
-    }
+        const soma = a+b
+        
+        let comparacao_soma = ''
+        
+        if(soma < 10){
+            comparacao_soma = 'menor que 10'
+        }else if(soma > 20){
+            comparacao_soma = 'maior que 20'
+        }else if(soma === 10){
+            comparacao_soma = 'igual a 10'
+        }else if(soma === 20){
+            comparacao_soma = 'igual a 20'
+        }else{
+            comparacao_soma = 'maior que 10 e menor que 20'
+        }
 
-    console.log(`Os números ${a} e ${b} ${comparacao_inputs}são iguais. Sua soma é ${soma}, que é ${comparacao_soma}.`)
+        console.log(`Os números ${a} e ${b} ${comparacao_inputs}são iguais. Sua soma é ${soma}, que é ${comparacao_soma}.`)
+    }else{
+        console.log("Escreva dois valores")
+    }
     
 }
 
-compararSomarComparar(4,5)
+compararSomarComparar(5,2)
