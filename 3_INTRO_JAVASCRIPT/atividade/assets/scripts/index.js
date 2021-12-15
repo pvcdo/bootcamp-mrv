de36c258572f1171976368c6acf124ab3b51b3cf
PAULO VICTOR CAETANO DE OLIVEIRA PR114067 <paulocivictor@gmail.com>
2021-12-15 15:21:55 -0300

    tarefa contador de 3_INTRO_JAVASCRIPT finalizada

---
+++ 3_INTRO_JAVASCRIPT/atividade/assets/scripts/index.js
@@ -0,0 +1,37 @@
+function subtrair(){
+    let count = parseFloat(document.getElementById('count').innerHTML)
+    if(count > -5){
+        count--
+        document.getElementById('count').innerHTML = count
+    }
+    verificaCor()
+}
+
+function somar(){
+    let count = parseFloat(document.getElementById('count').innerHTML)
+    if(count < 15){
+        count++
+        document.getElementById('count').innerHTML = count
+    }
+    verificaCor()
+}
+
+function load(){
+    let botao_menos = document.getElementById('btn-menos')
+    let botao_mais = document.getElementById('btn-mais')
+    let count = parseFloat(document.getElementById('count').innerHTML)
+
+    botao_menos.addEventListener('click',subtrair)
+    botao_mais.addEventListener('click',somar)
+
+    verificaCor()
+}
+
+function verificaCor(){
+    let count = parseFloat(document.getElementById('count').innerHTML)
+    if(count < 0){
+        document.getElementById('count').style.color = "red"
+    }else{
+        document.getElementById('count').style.color = "goldenrod"
+    }
+}
