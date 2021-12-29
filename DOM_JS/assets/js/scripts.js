f8b1e3efe7e9a4891dff64f1355ed3d2a6f6f715
PAULO VICTOR CAETANO DE OLIVEIRA PR114067 <paulocivictor@gmail.com>
2021-12-29 16:18:07 -0300

    aula pratica dom_js atualizada

--- DOM_JS/assets/js/scripts.js
+++ DOM_JS/assets/js/scripts.js
@@ -1,41 +1,22 @@
 function trocaModo(){
-    let valor = document.getElementById('hidden').value
+    body.classList.toggle('dark-mode')
+    footer.classList.toggle('dark-mode')
+    alteraTextos()
+}
 
-    if(valor == 0){
-        
-        //No Light mode trocando para dark
-
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#2F3337'
-        document.getElementsByTagName('body')[0].style.color = '#e5e8e8'
-
-        document.getElementsByTagName('footer')[0].style.backgroundColor = '#e5e8e8'
-        document.getElementsByTagName('footer')[0].style.color = '#2F3337'
-
-        document.getElementsByTagName('h2')[0].innerHTML = 'Olá! Estamos no Dark Mode'
-
-        document.getElementById('modo').innerHTML = '&#9789;'
-
-        document.getElementById('hidden').value = 1
+function alteraTextos(){
+    if(body.classList.contains('dark-mode')){
+        h2.innerHTML = "Olá! Estamos no Dark Mode"
+        botao.innerHTML = '&#9789;'
     }else{
-
-        //No Dark mode trocando pro light
-
-        document.getElementsByTagName('body')[0].style.backgroundColor = '#e5e8e8'
-        document.getElementsByTagName('body')[0].style.color = '#2F3337'
-
-        document.getElementsByTagName('footer')[0].style.backgroundColor = '#2F3337'
-        document.getElementsByTagName('footer')[0].style.color = '#e5e8e8'
-
-        document.getElementsByTagName('h2')[0].innerHTML = 'Olá! Estamos no Light Mode'
-
-        document.getElementById('modo').innerHTML = '&#9788;'
-
-        document.getElementById('hidden').value = 0
+        h2.innerHTML = "Olá! Estamos no Light Mode"
+        botao.innerHTML = '&#9788;'
     }
-
-    console.log(valor)
-    
 }
 
 const botao = document.getElementById('modo')
+const body = document.getElementsByTagName('body')[0]
+const footer = document.getElementsByTagName('footer')[0]
+const h2 = document.getElementsByTagName('h2')[0]
+
 botao.addEventListener('click',trocaModo)
