0e0d4a11f9042e57a07bc9e45d45eb8cd58100c9
Paulo Oliveira <paulocivictor@gmail.com>
2022-01-24 23:44:08 -0300

    iniciando aula de construtores em c#

---
+++ C#_Construtores/ExemploConstrutores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Paulo Victor", "Caetano de Oliveira");
+            p1.Apresentar();
+        }
+    }
+}
